463fa11b6c9b6c21c5637ad86a430a14ccc81bf3
ricomasgu <ricard.comas.gomez@gmail.com>
2022-09-07 10:24:19 +0200

    Seed created to start the database with a user to be able to check the results.

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -3,9 +3,11 @@ import { Schema } from 'mongoose';
 export const UserSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  //coordinates: [{ type: Schema.Types.ObjectId, ref: 'Coordinates' }],
 });
 
 export interface User {
   username: string;
   password: string;
+  //coordinates: [];
 }
